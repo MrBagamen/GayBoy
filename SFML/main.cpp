d0cf74961046f11aa4f2cf07e6aa982f8f9aa39d
Mika Attila <radiantstatue@gmail.com>
2013-11-28 19:10:41 +0100

    SFML: Show a retarded munching squares animation instead of random pixels

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -16,8 +16,14 @@ int main(int argc, char ** argv) {
                 window.close();
         }
         std::array<sf::Uint8, 160 * 144 * 4> pixels;
-        for (auto &p : pixels)
-            p = rand();
+        auto elapsedMilliseconds = clock.getElapsedTime().asMilliseconds();
+        for (int y = 0; y < 144; ++y)
+            for (int x = 0; x < 160; ++x) {
+                int i = y * (160 * 4) + x * 4;
+                sf::Uint8 &r = pixels[i], &g = pixels[i + 1], &b = pixels[i + 2], &a = pixels[i + 3];
+                r = g = b = (y ^ x) + elapsedMilliseconds;
+                a = 255;
+            }
         display.update(pixels.data());
         window.draw(sf::Sprite(display));
         window.display();
