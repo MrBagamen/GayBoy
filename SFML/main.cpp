e6f16a81705aec9953a3f906288b9ca248c3b44f
Mika Attila <radiantstatue@gmail.com>
2013-11-28 18:23:22 +0100

    SFML: Add scaleable display surface

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -1,16 +1,31 @@
 #include "gameboy.hpp"
 #include <SFML/Graphics.hpp>
+#include <array>
 
 int main(int argc, char ** argv) {
     gb::loadROM(argc > 1 ? argv[1] : "roms/Kirby's Dream Land.gb");
     sf::RenderWindow window(sf::VideoMode(160, 144), "GayBoy");
+    sf::Texture display;
+    display.create(160, 144);
+    int frames = 0;
+    sf::Clock clock;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear();
+        std::array<sf::Uint8, 160 * 144 * 4> pixels;
+        for (decltype(pixels)::size_type i = 0; i < pixels.size(); ++i)
+            pixels[i] = rand();
+        display.update(pixels.data());
+        window.draw(sf::Sprite(display));
         window.display();
+        ++frames;
+        if (clock.getElapsedTime() >= sf::seconds(1.0f)) {
+            clock.restart();
+            window.setTitle("GayBoy running at " + std::to_string(frames) + " fps");
+            frames = 0;
+        }
     }
 }
